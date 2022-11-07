df1adc5b0550b069745b55aff88e477c3da5b9d7
MeaBoy656 <meaboy656@gmail.com>
2022-11-07 18:27:20 +0200

    welcome intro fix

--- src/components/Layout/Opening.js
+++ src/components/Layout/Opening.js
@@ -3,21 +3,23 @@ import classes from "./Opening.module.css";
 
 const Opening = (props) => {
   const { title, subTitle, welcome, imgUrl, logoUrl } = props.content.opening;
-  const splittedParagraphWelocme = welcome.split(';')
+  console.log(welcome);
+  const splittedParagraphWelocme = welcome.split(";");
+
   return (
     <div className={classes["opening-container"]}>
       <header className={classes.header}>
         <div className={classes.logo}></div>
-        <h1 className={`${"title"} ${classes['opening-title']}`}>{title}</h1>
+        <h1 className={`${"title"} ${classes["opening-title"]}`}>{title}</h1>
         <h3 className="subTitle">{subTitle}</h3>
-        <div className={classes.welcomeSection}>
-        <h2 className={classes.welcome}>{splittedParagraphWelocme[0]}</h2>
-        <h2 className={classes.welcome}>{splittedParagraphWelocme[1]}</h2>
-        </div>
       </header>
       <figure className={classes.figure}>
         <div className={classes.hero}></div>
       </figure>
+      <div className={classes.welcomeSection}>
+        <p className={classes.welcome}>{splittedParagraphWelocme[0]}</p>
+        {/* <h2 className={classes.welcome}>{splittedParagraphWelocme[1]}</h2> */}
+      </div>
     </div>
   );
 };
