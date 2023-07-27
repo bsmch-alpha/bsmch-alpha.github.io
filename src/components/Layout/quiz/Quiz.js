49a130340bcf0327d618232f8e77680e18ebec8b
Lior Cohen <mpl106mpl@gmail.com>
2023-07-27 12:03:15 +0300

    chnages: answers + results work, key bug fixed

--- src/components/Layout/quiz/Quiz.js
+++ src/components/Layout/quiz/Quiz.js
@@ -3,26 +3,66 @@ import classes from "./Quiz.module.css"
 import quizContent from "../../../quiz-content.json"
 import Arrow from "../../SVG/Arrow";
 import { useHistory, useParams, useRouteMatch } from "react-router-dom";
-import ReactGA from "react-ga";
+import ReactGA, { set } from "react-ga";
 
 const CoursesFullContent = (props) => {
 
-  const [questionNum, setQuestionNum] = useState(0)
+  const questionObjects = quizContent.questions;
+  const results = quizContent.results;
 
-  const clickOnAnswer = () => {
-    setQuestionNum(prev => prev + 1)
+  const [questionNum, setQuestionNum] = useState(0);
+  let questionNumber = 0;
+
+  let quizResult = 0 ;
+  const [result, setResult] = useState();
+  const [resultText, setResultText] = useState();
+
+  const [syber, setSyber] = useState(0);
+  const [devops, setDevops] = useState(0);
+  const [programming, setProgramming] = useState(0);
+  const [data, setData] = useState(0);
+
+  const upSyber = () => {setSyber(prev => prev + 1)}
+  const upDevOps = () => {setDevops(prev => prev + 1)}
+  const upProgramming = () => {setProgramming(prev => prev + 1)}
+  const upData = () => {setData(prev => prev + 1)}
+
+  const functionsArray = [upSyber, upDevOps, upProgramming, upData];
+
+  const clickOnAnswer = (answer) => {
+    setQuestionNum(prev => prev + 1);
+    questionNumber = questionNum;
+    functionsArray[answer]();
+    (questionNumber + 1) === questionObjects.length && checkResult();
   }
+
+  const checkResult = () => {
+    if (syber >= devops && syber >= programming && syber >= data) {
+      quizResult = 1;
+    } else if (devops >= syber && devops >= programming && devops >= data) {
+      quizResult = 2;
+    } else if (programming >= syber && programming >= devops && programming >= data) {
+      quizResult = 3;
+    } else {
+      quizResult = 4;
+    }
+
+    setResult(results[quizResult].result);
+    setResultText(results[quizResult].text);
+  }
+
+  //Quiz Objects
 
   const createAnswers = (answers) => {
 
     let answerObjects = answers.map((answer, index) => (
-      <div className={classes["answer"]} onClick={() => clickOnAnswer()}>{answer}</div>
+      <div className={classes["answer"]} onClick={() => clickOnAnswer(index)}>{answer}</div>
     ))
 
     return answerObjects
   }
 
-  const questionsArray = quizContent.questions.map((question, index) => (
+  const questionsArray = questionObjects.map((question, index) => (
     <div className={classes["qustion-container"]} style={{color : "white"}}>
       <p className={classes["question-text"]}>{question.text}</p>
       <div className={classes["answers-container"]}>
@@ -41,6 +81,8 @@ const CoursesFullContent = (props) => {
     props.setIsQuizOpen(false);
   };
 
+  //
+
   return (
     <article className={classes["article-container"]}>
         <span
@@ -48,41 +90,10 @@ const CoursesFullContent = (props) => {
         onClick={exitModalClickHandler}
       ></span>
       {questionsArray[questionNum]}
-      {/* {content && (
-        <>
-          {/* <iframe
-            className={classes["article-image"]}
-            src={content.videoUrl}
-            loading="lazy"
-          ></iframe> */}
-          {/* <article>
-            <div className={classes["brief"]}>
-              <h3 className="sectionTitle">{content.title}</h3>
-              <p className="text">{content.brief}</p>
-            </div>
-            <ul className={`text ${classes["about-role"]}`}>{liElements}</ul>
-          </article>
-          <CoursesFullGallary content={content} />
-        </> */}
-      {/* )}
-      <span
-        className={classes["close-btn"]}
-        onClick={exitModalClickHandler}
-      ></span>
-      {!content && (
-        <>
-          <h1 className="cartTitle">
-            מצטערים, לא הצלחנו למצוא את מה שאתם מחפשים.
-          </h1> */}
-          {/* <h3 className="cartTitle">אבל הנה שיר  שיררגיע אתכם: </h3>
-          <iframe
-            className={classes["article-image"]}
-            src="https://www.youtube.com/embed/tgbNymZ7vqY"
-            loading="lazy"
-          ></iframe>
-          <div></div> */}
-        {/* </>
-      )} */} 
+      <div className={classes["result-container"]}>
+        <div className={classes["result-title"]} >{result}</div>
+        <div className={classes["result-text"]} >{resultText}</div>
+      </div>
     </article>
   );
 };
